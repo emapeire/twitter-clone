bc2ef68fd0e99bc6f4df2225030ae89a07457e74
Emanuel Peire <emapeire@outlook.com>
2023-09-09 17:07:17 -0300

    add new features from supabase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
-export default function Home() {
+import { AuthButton } from '@/components/auth-button'
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+
+export default async function Home() {
+  const supabase = createServerComponentClient({ cookies })
+  const { data: posts } = await supabase.from('posts').select('*')
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hola Twitter 👋🏻
+      <AuthButton />
+      <pre>
+        {
+          JSON.stringify(posts, null, 2)
+        }
+      </pre>
     </main>
   )
 }
